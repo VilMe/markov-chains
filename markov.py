705b546ecbc98f96b93e8f384e11373300bbefca
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-02 20:17:05 -0700

    open file, read and return text as string

--- markov.py
+++ markov.py
@@ -4,77 +4,84 @@ from random import choice
 
 
 def open_and_read_file(file_path):
-    """Take file path as string; return text as string.
+	"""Take file path as string; return text as string.
 
-    Takes a string that is a file path, opens the file, and turns
-    the file's contents as one string of text.
-    """
+	Takes a string that is a file path, opens the file, and turns
+	the file's contents as one string of text.
+	"""
 
-    file_read = open(file_path, 'r')
-    file_str = file_read.read()
-    return file_str
+	file_read = open(file_path, 'r')
+	file_str = file_read.read()
+	return file_str
 
-    # return "Contents of your file as one long string"
+	# return "Contents of your file as one long string"
 
 
 def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains.
+	"""Take input text as string; return dictionary of Markov chains.
 
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
+	A chain will be a key that consists of a tuple of (word1, word2)
+	and the value would be a list of the word(s) that follow those two
+	words in the input text.
 
-    For example:
+	For example:
 
-        >>> chains = make_chains("hi there mary hi there juanita")
+		>>> chains = make_chains("hi there mary hi there juanita")
 
-    Each bigram (except the last) will be a key in chains:
+	Each bigram (except the last) will be a key in chains:
 
-        >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+		>>> sorted(chains.keys())
+		[('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
 
-    Each item in chains is a list of all possible following words:
+	Each item in chains is a list of all possible following words:
 
-        >>> chains[('hi', 'there')]
-        ['mary', 'juanita']
-        
-        >>> chains[('there','juanita')]
-        [None]
-    """
-    chains = {}
+		>>> chains[('hi', 'there')]
+		['mary', 'juanita']
+		
+		>>> chains[('there','juanita')]
+		[None]
+	"""
+	chains = {}
 
-    # your code goes here
-    print(text_string)
-    text_string = text_string.split()
+	# your code goes here
+	text_string = text_string.split()
 
-    for i in range(len(text_string) - 2):
-    	bigram = (text_string[i], text_string[i + 1])
-    	next_word = text_string[i + 2]
-    	
+	for i in range(len(text_string) - 2):
+		bigram = (text_string[i], text_string[i + 1])
+		next_word = text_string[i + 2]
+		if bigram not in chains:
+			chains[bigram] = []
+		chains[bigram].append(next_word)
+
+	return chains
 	
-    # return chains
 
 
 
-# def make_text(chains):
+def make_text(chains):
 #     """Return text from chains."""
 
-#     words = []
+	words = []
+	link = choice(list(chains.keys()))
 
-#     # your code goes here
+	while link in chains:
+		link_value = choice(chains[link])
+		words.extend(list(link))
+		words.append(link_value)
 
-#     return " ".join(words)
+		link = (link[1],) + (link_value,)
+	return " ".join(words)
 
 
-input_path = "green-eggs.txt"
+input_path = "gettysburg.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
-#Get a Markov chain
+# Get a Markov chain
 chains = make_chains(input_text)
 
 #Produce random text
-#random_text = make_text(chains)
+random_text = make_text(chains)
 
-#print(random_text)
+print(random_text)
